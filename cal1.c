85d82f6735292bf9d11c0590fde7bc8e5f85115f
yogeshwarrj <56185951+yogeshwarrj@users.noreply.github.com>
2019-10-30 20:08:43 +0530

    Updatecal1.c
    values take from user

--- cal1.c
+++ cal1.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 void main()
 {
-int n,a=10,b=5;
-printf("Enter th choice 1 for addition\n 2 for sub\n ");
+int n,a,b;
+printf("enter the value of a and b");
+scanf("%d %d",&a,&b);
+printf("Enter th choice 1 for addition\n 2 for sub\n 3 for mul\n 4 for div");
 scanf("%d",&n);
 switch(n)
 {
 case 1: printf("addition= %d",a+b);
         break;
 case 2: printf("sub=%d",a-b);
+        break;
+case 3: printf("mul=%d",a*b);
+        break;
+case 4: printf("div=%d",a/b);
         break;
 }
 }
